ebb2ba44bf4311f576d1b114deb3bc03eb975ddf
Gregory Row <rowgregory@gmail.com>
2026-04-01 16:18:21 -0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import "./globals.css";
+
+const lexend = Lexend({
+  variable: "--font-lexend",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Courtney Steele",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${lexend.variable} h-full antialiased`}>
+      <body className={`min-h-full flex flex-col`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="text-white uppercase">Courtney Steele</main>
+    </div>
+  );
+}
